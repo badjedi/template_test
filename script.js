b528023ea1f6b4425ede3b63e809b7a21a2b2d65
badjedi <badjedi@live.com>
2023-06-09 15:52:53 -0500

    Updated file.

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// let apiQuotes = [];
+ let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     console.log(quote);
 }
 
 
 // Get quotes from APi asynchronous fetch request
-// async function getQuotes() {
-//     const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
-//     try {
-//         const response = await fetch(apiUrl);
-//         apiQuotes = await response.json();
-//         newQuote();
-//     } catch (error) {
-//         //  Catch Error Here
-//     }
-// }
+     async function getQuotes() {
+         const apiUrl = "https://zenquotes.io/";
+        try {
+            const response = await fetch(apiUrl);
+            apiQuotes = await response.json();
+            newQuote();
+     } catch (error) {
+        //   Catch Error Here
+     }
+ }
 
-// // On Load
-// getQuotes();
-newQuote();
+ // On Load
+getQuotes();
+// newQuote();
