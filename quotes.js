b9b309c391bf9917db45a0c7d87e7d43d7bfa1cb
badjedi <badjedi@live.com>
2023-06-09 13:57:27 -0500

    Updated javaScript files.

---
+++ quotes.js
@@ -0,0 +1,139 @@
+const localQuotes = [
+{
+	text: 'Don't wait until you've reached your goal to be proud of yourself. Be proud of every step you take toward reaching that goal.',
+	author: 'Simone Biles',
+},
+{
+	text: 'I realized that I don't have to be perfect. All I have to do is show up and enjoy the messy, imperfect, and beautiful journey of my life.',
+	author: 'Kerry Washington',
+},
+{
+	text: 'Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world.',
+	author: 'Harriet Tubman',
+},
+{
+	text: 'I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.',
+	author: 'Maya Angelou',
+},
+{
+	text: 'Don't settle for average. Bring your best to the moment. Then, whether it fails or succeeds, at least you know you gave all you had. We need to live the best that's in us.',
+	author: 'Angela Bassett',
+},
+{
+	text: 'I have learned over the years that when one's mind is made up, this diminishes fear; knowing what must be done does away with fear.',
+	author: 'Rosa Parks',
+},
+{
+	text: 'When I dare to be powerful — to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.',
+	author: 'Audre Lorde',
+},
+{
+	text: 'Embrace what makes you unique, even if it makes others uncomfortable. I didn't have to become perfect because I've learned throughout my journey that perfection is the enemy of greatness.',
+	author: 'Janelle Monae',
+},
+{
+	text: 'Your willingness to look at your darkness is what empowers you to change.',
+	author: 'Iyanla Vanzant',
+},
+{
+	text: 'Failure is an important part of your growth and developing resilience. Don’t be afraid to fail.',
+	author: 'Michelle Obama',
+},
+{
+	text: 'The most significant successes come from letting your light shine, embracing failure, and getting good at being wrong.',
+	author: 'Stacey Abrams',
+},
+{
+	text: 'We should seek out all the doors which still remain ajar, however slight the opening might be.',
+	author: 'Angela Davis',
+},
+{
+	text: 'When you are willing to make sacrifices for a great cause, you will never be alone.',
+	author: 'Coretta Scott King',
+},
+{
+	text: 'Defeat should not be the source of discouragement, but a stimulus to keep plotting.',
+	author: 'Shirley Chisolm',
+},
+{
+	text: 'Know that you can start late, look different, be uncertain and still succeed.',
+	author: 'Misty Copeland',
+},
+{
+	text: 'If we give our children sound self-love, they will be able to deal with whatever life puts before them.',
+	author: 'bell hooks',
+},
+{
+	text: 'You've got to learn to leave the table when love's no longer being served.',
+	author: 'Nina Simone',
+},
+{
+	text: 'Deal with yourself as an individual worthy of respect, and make everyone else deal with you the same way.',
+	author: 'Nikki Giovanni',
+},
+{
+	text: 'You have to believe in yourself when no one else does.',
+	author: 'Serena Williams',
+},
+{
+	text: 'You’ve just got to follow your own path. You have to trust your heart and you have to listen to the warnings.',
+	author: 'Chaka Khan',
+},
+{
+	text: 'If you are silent about your pain, they’ll kill you and say you enjoyed it.',
+	author: 'Zora Neale Hurston',
+},
+{
+	text: 'Never be limited by other people's limited imaginations.',
+	author: 'Dr. Mae Jemison',
+},
+{
+	text: 'Defining myself, as opposed to being defined by others, is one of the most difficult challenges I face.',
+	author: 'Carol Moseley-Braun',
+},
+{
+	text: 'Just don't give up what you're trying to do. Where there is love and inspiration, I don't think you can go wrong.',
+	author: 'Ella Fitzgerald',
+},
+{
+	text: 'Truth is powerful and it prevails.',
+	author: 'Sojourner Truth',
+},
+{
+	text: 'Whatever is bringing you down, get rid of it. Because you’ll find that when you’re free . . . your true self comes out..',
+	author: 'Tina Turner',
+},
+{
+	text: 'The way to right wrongs is to turn the light of truth upon them.',
+	author: 'Ida B. Wells',
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
