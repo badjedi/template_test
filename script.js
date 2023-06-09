c013557dcf182130d02528ff774e9a02ab8b5479
badjedi <badjedi@live.com>
2023-06-09 17:02:25 -0500

    Updated all files.

--- script.js
+++ script.js
@@ -3,9 +3,24 @@
  const authorText  = document.getElementById('author');
  const twitterBtn  = document.getElementById('twitter');
  const newQuoteBtn  = document.getElementById('new-quote');
+ const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
+
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+
 
 // Show New Quote
 function newQuote() {
@@ -18,7 +33,7 @@ function newQuote() {
         authorText.textContent = quote.author;
     }
 //  Check quote length to determine the styling
-    if (quote.text.length > 50) {
+    if (quote.text.length > 120) {
         quoteText.classList.add('long-quote');
     } else{
         quoteText.classList.remove('long-quote');
@@ -38,7 +53,21 @@ function newQuote() {
         //   Catch Error Here
      }
  }
+// Tweet Quote
+// it uses backticks under the escape key
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 
  // On Load
-getQuotes();
+// getQuotes();
 // newQuote();
+loading();
